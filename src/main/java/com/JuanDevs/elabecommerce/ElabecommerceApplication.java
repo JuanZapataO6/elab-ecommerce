e4f903b572c85eb78c583f2e7391d439a3adc098
JuanZapataO6 <juan.zapataoc@gmail.com>
2023-07-20 16:52:57 -0500

    First Commit

---
+++ src/main/java/com/JuanDevs/elabecommerce/ElabecommerceApplication.java
@@ -0,0 +1,13 @@
+package com.JuanDevs.elabecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElabecommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElabecommerceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/JuanDevs/elabecommerce/ElabecommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.JuanDevs.elabecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElabecommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
